ca7ed5d4e122c162a3c0f911f7d5770ca41b44b7
IrinaBond <irina.karavdina@gmail.com>
2019-07-04 15:33:08 +0300

    Защита
    Добавлены минимизированные и форматированные стили и скрипты

--- js/popup.js
+++ js/popup.js
@@ -1,1 +1,60 @@
-var link=document.querySelector(".button-write-us"),popup=document.querySelector(".modal-write-us"),close=document.querySelector(".modal-close"),form=popup.querySelector(".appointment-form"),guestname=popup.querySelector("[name=guestname]"),guestemail=popup.querySelector("[name=guestemail]"),isStorageSupport=!0,nameFromStorage="",emailFromStorage="",guestcomment=popup.querySelector("[name=comment]"),overlay=document.querySelector(".overlay");try{nameFromStorage=localStorage.getItem("guestname"),emailFromStorage=localStorage.getItem("guestemail")}catch(e){isStorageSupport=!1}link.addEventListener("click",function(e){e.preventDefault(),popup.classList.add("modal-show"),overlay.classList.add("overlay-show"),nameFromStorage&&emailFromStorage?(guestname.value=nameFromStorage,guestemail.value=emailFromStorage,guestcomment.focus()):guestname.focus()}),close.addEventListener("click",function(e){e.preventDefault(),popup.classList.remove("modal-show"),overlay.classList.remove("overlay-show")}),form.addEventListener("submit",function(e){guestname.value&&guestemail.value?isStorageSupport&&(localStorage.setItem("guestname",guestname.value),localStorage.setItem("guestemail",guestemail.value)):e.preventDefault()}),window.addEventListener("keydown",function(e){27===e.keyCode&&popup.classList.contains("modal-show")&&(e.preventDefault(),popup.classList.remove("modal-show"),overlay.classList.remove("overlay-show"))});
+  var link = document.querySelector(".button-write-us");
+  var popup = document.querySelector(".modal-write-us");
+  var close = document.querySelector(".modal-close");
+  var form = popup.querySelector(".appointment-form");
+  var guestname = popup.querySelector("[name=guestname]");
+  var guestemail = popup.querySelector("[name=guestemail]");
+  var isStorageSupport = true;
+  var nameFromStorage = "";
+  var emailFromStorage = "";
+  var guestcomment = popup.querySelector("[name=comment]");
+  var overlay = document.querySelector(".overlay");
+
+  try {
+    nameFromStorage = localStorage.getItem("guestname");
+    emailFromStorage = localStorage.getItem("guestemail");
+  } catch (err) {
+    isStorageSupport = false;
+  }
+  
+  link.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.add("modal-show");
+    overlay.classList.add("overlay-show");
+    if (nameFromStorage && emailFromStorage) {
+      guestname.value = nameFromStorage;
+      guestemail.value = emailFromStorage;
+      guestcomment.focus();
+    } else{
+      guestname.focus();
+    }
+  });
+
+  close.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("modal-show");
+    overlay.classList.remove("overlay-show");
+
+  });
+
+  form.addEventListener("submit", function (evt) {
+    if (!guestname.value || !guestemail.value) {
+      evt.preventDefault();
+      // TODO: обработка случая с пустой формой
+    } else {
+      if (isStorageSupport) {
+        localStorage.setItem("guestname", guestname.value);
+        localStorage.setItem("guestemail", guestemail.value);
+      }
+    }
+  });
+
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      if (popup.classList.contains("modal-show")){
+        evt.preventDefault();
+        popup.classList.remove("modal-show");
+        overlay.classList.remove("overlay-show");
+      }
+    }
+  })
--- js/slider.js
+++ js/slider.js
@@ -1,1 +1,36 @@
-var controls1=document.querySelector(".controls1"),controls2=document.querySelector(".controls2"),controls3=document.querySelector(".controls3"),slide1=document.querySelector(".slide1"),slide2=document.querySelector(".slide2"),slide3=document.querySelector(".slide3");controls1.addEventListener("click",function(s){s.preventDefault(),slide1.classList.add("slide-show"),slide2.classList.remove("slide-show"),slide3.classList.remove("slide-show"),controls1.classList.add("active"),controls2.classList.remove("active"),controls3.classList.remove("active")}),controls2.addEventListener("click",function(s){s.preventDefault(),slide1.classList.remove("slide-show"),slide2.classList.add("slide-show"),slide3.classList.remove("slide-show"),controls1.classList.remove("active"),controls2.classList.add("active"),controls3.classList.remove("active")}),controls3.addEventListener("click",function(s){s.preventDefault(),slide1.classList.remove("slide-show"),slide2.classList.remove("slide-show"),slide3.classList.add("slide-show"),controls1.classList.remove("active"),controls2.classList.remove("active"),controls3.classList.add("active")});
+  var controls1 = document.querySelector(".controls1");
+  var controls2 = document.querySelector(".controls2");
+  var controls3 = document.querySelector(".controls3");
+  var slide1 = document.querySelector(".slide1");
+  var slide2 = document.querySelector(".slide2");
+  var slide3 = document.querySelector(".slide3");
+  
+  controls1.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    slide1.classList.add("slide-show");
+    slide2.classList.remove("slide-show");
+    slide3.classList.remove("slide-show");
+    controls1.classList.add("active");
+    controls2.classList.remove("active");
+    controls3.classList.remove("active");
+  })
+
+  controls2.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    slide1.classList.remove("slide-show");
+    slide2.classList.add("slide-show");
+    slide3.classList.remove("slide-show");
+    controls1.classList.remove("active");
+    controls2.classList.add("active");
+    controls3.classList.remove("active");  
+})
+
+  controls3.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    slide1.classList.remove("slide-show");
+    slide2.classList.remove("slide-show");
+    slide3.classList.add("slide-show");
+    controls1.classList.remove("active");
+    controls2.classList.remove("active");
+    controls3.classList.add("active");
+  })
